c067949833528fb316042632b772b44fbc9fe023
luanjampa <luansouza.jampa@gmail.com>
2013-07-12 17:14:16 -0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,16 +1,16 @@
 import urllib.request
-print("Exemple URL: http://google.com.br")
-domain = input("URL: ") 
-listdir = open("FILE TXT HERE", "r") # Open file
+
+dominio = input("URL: ")
+listdir = open("dictionary.txt", "r")
 for line in listdir.readlines():
-    url = domain + '/' + line.strip()
+    url = dominio + '/' + line.strip()
     try:
         site = urllib.request.urlopen(url)
         status = site.getcode()
         if status == 200:
-            print (dommain + '/' + line.strip(),"OK")
-        elif status == 302:
-            print (domain + '/' + line.strip(),"REDIR")
+            print (dominio + '/' + line.strip(),"OK")
+        elif status == 200:
+            print (dominio + '/' + line.strip(),"OK")
         
     except:
-        site.status == 404 # Remove pages with 404 error
+        site.status == 404 # Remover paginas 404
