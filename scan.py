a10a5e7819afb321a6290eed2693e0d2db7f10ed
luanjampa <luansouza.jampa@gmail.com>
2013-07-16 18:33:32 -0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python
+
 import urllib.request
 
-dominio = input("URL: ")
-listdir = open("dictionary.txt", "r")
-for line in listdir.readlines():
+dominio = input("Informe a url: ")
+listdir = open("spse2.txt", "r")
+for line in listdir.readlines(): # pega cada linha do arquivo do listdir
     url = dominio + '/' + line.strip()
     try:
         site = urllib.request.urlopen(url)
         status = site.getcode()
         if status == 200:
             print (dominio + '/' + line.strip(),"OK")
-        elif status == 302
-            print (dominio + '/' + line.strip(),"REDIR")
+        elif status == 302:
+            print (dominio + '/' + line.strip(),"Redir")
         
     except:
         site.status == 404 # Remover paginas 404
+            
+        
+        
